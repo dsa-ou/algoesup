32759869c5250952d451a402348939ae10643457
michael <m.densnow@gmail.com>
2024-02-27 01:32:39 +0000

    Add google style docstrings
    - Add google style docstrings to main algoesup library functions with
      extra descriptions and information on parameters.
    - Remove conflicting md from library.md: module docstrings are parsed
      and used (can be changed to suit tastes).
    - Update config files to parse google style docstings.
    - Update config files for new location of algoesup source files for doc
      parsing.

--- src/algoesup/test.py
+++ src/algoesup/test.py
@@ -1,4 +1,4 @@
-"""Simplified testing for functions."""
+"""Simplified testing for Python functions"""
 
 from typing import Callable
 
@@ -6,10 +6,11 @@ from typing import Callable
 def test(function: Callable, test_table: list) -> None:
     """Test the function with the test_table. Report failed tests.
 
-    Preconditions: each element of test_table is a list or tuple with
-        - a string (the test case name)
-        - one or more values (the inputs to the function)
-        - the expected output value
+    Args:
+        function (Callable): The function to be tested.
+        test_table (list): The list of tests. Each element of test_table is a list or 
+            tuple with: a string (the test case name); one or more values (the inputs to the function);
+            the expected output value.
     """
     print(f"Testing {function.__name__}:")
     for test_case in test_table:
