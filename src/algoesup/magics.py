fd5a6617cdac9e5ab38cacfb0d425a8bf2b5c6fc
Michel Wermelinger <michel.wermelinger@open.ac.uk>
2024-03-14 11:32:15 +0000

    Update documentation

--- src/algoesup/magics.py
+++ src/algoesup/magics.py
@@ -97,21 +97,16 @@ def process_status(name: str, status: str) -> None:
 )
 @register_line_magic
 def pytype(line: str) -> None:
-    """Activate/deactivate the `pytype` linter.
-    
-    This ipython magic command controls the activation state of the `pytype` linter within
-    the ipython environment. It can be toggled on or off, or queried for its
-    current state.
+    """Activate/deactivate the [pytype linter](https://google.github.io/pytype).
 
-    Examples:
-        ```
-        %pytype on 
-        pytype was activated
-        %pytype off     
-        pytype was deactivated
-        %pytype         
-        pytype is inactive  
-        ```
+    When active, the linter checks each code cell that is executed for type errors.
+
+    - `%pytype --disable ... on` activates the linter but does not check the given errors
+      (see the [list of errors](https://google.github.io/pytype/errors.html))
+    - `%pytype on` is equal to `%pytype --disable name-error,import-error on`
+    - `%pytype off` deactivates the linter
+    - `%pytype` shows the current status of the linter
+    - `%pytype?` shows this documentation and the command's options
     """
     args = parse_argstring(pytype, line)
     checkers["pytype"][0] = ["pytype", "--disable", args.disable]
@@ -135,21 +130,18 @@ def pytype(line: str) -> None:
 )
 @register_line_magic
 def allowed(line: str) -> None:
-    """Activate/deactivate the `allowed` linter.
+    """Activate/deactivate the [allowed linter](https://dsa-ou.github.io/allowed).
       
-    This ipython magic command controls the activation state of the `allowed` linter 
-    within the ipython environment. It can be toggled on or off, or queried for its
-    current state.
-  
-    Examples:
-        ```
-        %allowed on 
-        pytype was activated
-        %allowed off     
-        pytype was deactivated
-        %allowed        
-        pytype is inactive
-        ```
+    When active, the linter checks each code cell that is executed for any
+    Python constructs that are not listed in the given configuration file.
+
+    - `%allowed --config ... on` activates the linter with the given configuration,
+      which must be `m269.json`, `tm112.json` or 
+      [one you defined](https://dsa-ou.github.io/allowed/docs/configuration.html)
+    - `%allowed on` is equal to `%allowed --config m269.json on`
+    - `%allowed off` deactivates the linter
+    - `%allowed` shows the current status of the linter
+    - `%allowed?` shows this documentation and the command's options
     """
     args = parse_argstring(allowed, line)
     if args.config:
@@ -180,21 +172,17 @@ def allowed(line: str) -> None:
 )
 @register_line_magic
 def ruff(line: str) -> None:
-    """Activate/deactivate the `ruff` linter.
+    """Activate/deactivate the [Ruff linter](https://docs.astral.sh/ruff).
+
+    When active, the linter checks each code cell that is executed
+    against the selected code style rules.
     
-    This ipython magic command controls the activation state of the `ruff` linter within
-    the ipython environment. It can be toggled on or off, or queried for its
-    current state.
-   
-    Examples:
-        ```
-        %ruff on
-        ruff was activated
-        %ruff off
-        ruff was deactivated
-        %ruff
-        ruff is inactive
-        ```
+    - `%ruff --select ... --ignore ... on` activates the linter with the given rules
+      (see [the list of rules](https://docs.astral.sh/ruff/rules))
+    - `%ruff on` is equal to `%ruff --select A,B,C90,D,E,W,F,N,PL --ignore D100,W292,F401,F821,D203,D213,D415 on`
+    - `%ruff off` deactivates the linter
+    - `%ruff` shows the current status of the linter
+    - `%ruff?` shows this documentation and the command's options
     """
     args = parse_argstring(ruff, line)
     base = ["ruff", "check", "--output-format", "json"]
