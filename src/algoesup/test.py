18cf36cd19e0c01cf15152780ada75494702ebd2
michael <m.densnow@gmail.com>
2024-04-12 18:40:29 +0100

    Make test more robust
    Add function validate_test_table to confirm:
     * the table is list or tuple
     * each case is list or tuple
     * first element of each case is str
     * each case has correct number of args

--- src/algoesup/test.py
+++ src/algoesup/test.py
@@ -3,6 +3,24 @@
 from typing import Callable
 
 
+def validate_test_table(function: Callable, test_table: list) -> list:
+    """Validate the structure of a test table"""
+    errors = []
+    if not isinstance(test_table, (list, tuple)):
+        errors.append("The test table must be a list or tuple.")
+        return errors
+    # The number of arguments function expects.
+    expects = function.__code__.co_argcount
+    for num, test_case in enumerate(test_table, start=1):
+        if not isinstance(test_case, (list, tuple)):
+            errors.append(f"Test case {num}: should be a list or tuple.")
+        elif not isinstance(test_case[0], str):
+            errors.append(f"Test case {num}: first element must be a string.")
+        elif len(test_case[1:-1]) != expects:
+            num_args = len(test_case[1:-1])
+            errors.append(f"Test case {num}: {num_args} inputs found instead of {expects}")
+    return errors
+    
 def test(function: Callable, test_table: list) -> None:
     """Test the function with the test_table. Report failed tests.
 
@@ -12,6 +30,13 @@ def test(function: Callable, test_table: list) -> None:
             tuple with: a string (the test case name); one or more values (the inputs to the function);
             the expected output value.
     """
+    if errors := validate_test_table(function, test_table):
+        print("ERROR: invalid test table:")
+        for error in errors:
+            print(error)
+        print(f"{function.__name__} has NOT been tested.")
+        return
+
     print(f"Testing {function.__name__}...")
     passed = failed = 0
     for test_case in test_table:
